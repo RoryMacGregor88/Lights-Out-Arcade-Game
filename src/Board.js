f7ab60a071a66109ce02178c0bd7347febc810e4
RoryMacGregor88 <RoryMacGregor88@outlook.com>
2019-08-06 20:04:25 +0100

    Added files and Board.js layout.

---
+++ src/Board.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Cell from './Cell';
+import './Board.css';
+
+class Board extends Component {
+    constructor(props) {
+        super(props){
+    }
+    render() { 
+        return ( 
+
+         );
+    }
+
+    createBoard() {
+        let board = [];
+
+        return board;
+    }
+
+    flipCellsAround(coord) {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <table>
+                    <tbody>
+                        <tr></tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+ 
+export default Board;
