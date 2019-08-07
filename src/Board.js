8443fe04b89e256e5d05bdd509d6b93c8418b9c2
RoryMacGregor88 <RoryMacGregor88@outlook.com>
2019-08-07 16:42:09 +0100

    Copiled Sass to main css file.

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Cell from './Cell';
-import './sass/main.scss';
+import '../src/style.css';
 
 class Board extends Component {
     static defaultProps = {
