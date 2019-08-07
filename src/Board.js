913732ec5d59ec010ff0c0e60a5e29320af532b7
RoryMacGregor88 <RoryMacGregor88@outlook.com>
2019-08-07 01:08:05 +0100

    Flip coordinates functionality completed.

--- src/Board.js
+++ src/Board.js
@@ -3,34 +3,82 @@ import Cell from './Cell';
 import './sass/main.scss';
 
 class Board extends Component {
+    static defaultProps = {
+        nrows: 5,
+        ncols: 5,
+        chanceLightStartsOn: 0.25
+    }
     constructor(props) {
-        super(props)
+        super(props);
+
+        this.state = {
+            hasWon: false,
+            board: this.createBoard()
+        }
     }
 
     createBoard() {
         let board = [];
 
+        for (let y = 0; y < this.props.nrows; y++) {
+            let row = [];
+            for (let x = 0; x < this.props.ncols; x++) {
+                row.push(Math.random() < this.props.chanceLightStartsOn);
+            }
+            board.push(row);
+        }
+
         return board;
     }
 
     flipCellsAround(coord) {
+        let {ncols, nrows} = this.props;
+        let board = this.state.board;
+        let [y, x] = coord.split('-').map(Number);
+        let hasWon = false;
 
+        function flipCell(y, x) {
+            
+            if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+                board[y][x] = !board[y][x];
+            }
+        }
+
+        flipCell(x, y);
+        flipCell(x-1, y);
+        flipCell(x, y-1);
+        flipCell(x, y+1);
+        flipCell(x+1, y);
+
+        this.setState({board, hasWon});
     }
 
     render() {
+
+        let tblBoard = [];
+        for (let y = 0; y < this.props.nrows; y++) {
+            let row = [];
+            for (let x = 0; x < this.props.ncols; x++) {
+                let coord = `${x}-${y}`
+                row.push(
+                    <Cell 
+                        key={coord} 
+                        isLit={this.state.board[y][x]} 
+                        flipCellsAroundMe={() => this.flipCellsAround(coord)}
+                    />)   
+            }
+            tblBoard.push(<tr key={y}>{row}</tr>)
+        }
+
         return (
-                <table className='Board'>
+            <div>
+                <h1>BOARD</h1>
+                <table className='board'>
                     <tbody>
-                        <tr>
-                            <Cell isLit={true} />
-                            <Cell isLit={false} />
-                            <Cell isLit={true} />
-                            <Cell isLit={false} />
-                            <Cell isLit={true} />
-                            <Cell isLit={true} />
-                        </tr>
+                        {tblBoard}
                     </tbody>
                 </table>
+            </div>
         )
     }
 }
